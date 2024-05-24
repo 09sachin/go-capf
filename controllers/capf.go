cdbeb08dcb17b51ac9e50ecc7f1924b79314afbb
09sachin <sachinkhoja18@gmail.com>
2024-05-24 13:08:00 +0530

    query

--- controllers/capf.go
+++ controllers/capf.go
@@ -642,6 +642,8 @@ func UserClaims(w http.ResponseWriter, r *http.Request) {
 	}
 
 	rows, sql_error := config.ExecuteQuery(claims_query, args...)
+	InfoLogger.Println(rows)
+	InfoLogger.Println(args)
 	if sql_error != nil {
 		ErrorLogger.Println(sql_error)
 		DbError(w)
