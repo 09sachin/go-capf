f18e48b59009c3d9b2f2e91a782f0ccd3bbb6066
09sachin <49692211+09sachin@users.noreply.github.com>
2025-05-15 13:07:21 +0530

    Update const.go

--- controllers/const.go
+++ controllers/const.go
@@ -8,8 +8,8 @@ var (
 	InfoLogger  *log.Logger
 	ErrorLogger *log.Logger
 	Beneficiary_URL = "https://apisprod.nha.gov.in/pmjay/prodbis/capfService/v3.0/searchFamilyDetails"
-	SMS_Username = getEnv("SMS_USER")
-	SMS_Password = getEnv("SMS_PASS")
+	SMS_Username = "PMJCAPF"
+	SMS_Password = "pybnZ4tD"
 	CLAIMS_UPDATE_BASE_URL = getEnv("CLAIMS_UPDATE_BASE_URL")
 	PMJAY_CLIENT_ID = getEnv("PMJAY_CLIENT_ID")
 	PMJAY_CLIENT_SECRET = getEnv("PMJAY_CLIENT_SECRET")
